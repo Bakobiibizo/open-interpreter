3e60e315f5b162fbf98b0b68f0f2e9a5a4e6961f
Richard <bakobiibizo@gmail.com>
2023-09-24 14:39:32 -0700

    completed the interperter models

--- interpreter/core/core.py
+++ interpreter/core/core.py
@@ -1,5 +1,5 @@
 """
-This file defines the Interpreter class.
+This file defines the abstract base class and base model classes for the interpreter class. the abstract base class is used to define the types of functions that the interpreter class will have and the model defines how that model will interact with data. 
 It's the main file. `import interpreter` will import an instance of this class.
 """
 from interpreter.utils import display_markdown_message
@@ -15,102 +15,79 @@ import json
 from datetime import datetime
 from ..utils.check_for_update import check_for_update
 from ..utils.display_markdown_message import display_markdown_message
+from pydantic import BaseModel
+from ABC import ABC
+from ABC import abstractmethod
 
-class Interpreter:
-    def cli(self):
-        cli(self)
 
-    def __init__(self):
-        # State
-        self.messages = []
-        self._code_interpreters = {}
-
-        # Settings
-        self.local = False
-        self.auto_run = False
-        self.debug_mode = False
-        self.max_output = 2000
-
-        # Conversation history
-        self.conversation_history = True
-        self.conversation_name = datetime.now().strftime("%B_%d_%Y_%H-%M-%S")
-        self.conversation_history_path = os.path.join(appdirs.user_data_dir("Open Interpreter"), "conversations")
-
-        # LLM settings
-        self.model = ""
-        self.temperature = 0
-        self.system_message = ""
-        self.context_window = None
-        self.max_tokens = None
-        self.api_base = None
-        self.api_key = None
-        self.max_budget = None
-        self._llm = None
-
-        # Load config defaults
-        config = get_config()
-        self.__dict__.update(config)
-
-        # Check for update
-        if not self.local:
-            # This should actually be pushed into the utility
-            if check_for_update():
-                display_markdown_message("> **A new version of Open Interpreter is available.**\n>Please run: `pip install --upgrade open-interpreter`\n\n---")
-
-    def chat(self, message=None, display=True, stream=False):
-        if stream:
-            return self._streaming_chat(message=message, display=display)
-        
-        # If stream=False, *pull* from the stream.
-        for _ in self._streaming_chat(message=message, display=display):
-            pass
-        
-        return self.messages
+class InterpreterModel(BaseModel):
     
-    def _streaming_chat(self, message=None, display=True):
+       # State
+    messages: List
+    _code_interpreters: Dict
 
-        # If we have a display,
-        # we can validate our LLM settings w/ the user first
-        if display:
-            validate_llm_settings(self)
+    # Settings
+    local: bool
+    auto_run: bool
+    debug_mode: bool
+    max_output: int
 
-        # Setup the LLM
-        if not self._llm:
-            self._llm = setup_llm(self)
+    # Conversation history
+    conversation_history: bool
+    conversation_name: str
+    conversation_history_path: str
 
-        # Sometimes a little more code -> a much better experience!
-        # Display mode actually runs interpreter.chat(display=False, stream=True) from within the terminal_interface.
-        # wraps the vanilla .chat(display=False) generator in a display.
-        # Quite different from the plain generator stuff. So redirect to that
-        if display:
-            yield from terminal_interface(self, message)
-            return
+    # LLM settings
+    model: str
+    temperature: int
+    system_message: str
+    context_window: Union[str, None]
+    max_tokens: Union[int, None]
+    api_base: Union[str, None]
+    api_key: Union[str, None]
+    max_budget: Union[int, None]
+    _llm: Union[str, None]
+    
+    def __init__(self, **data):
+        super().__init__(**data)
+    
+    # Load config defaults
+    config = self.get_configuration_file()
+    
+    #turns the config file contents into a dictionary for easy access
+    # should consider another method for setting these values rather than directly updating __dict__
+    __dict__.update(config)
+
+    def get_configuration_file(self) -> str:
+        """This method should be implemented in the concrete class and should return the config as a string."""
+        raise NotImplementedError("You must implement this method in the concrete class.")
+
+class InterpreterABC(ABC):
+    @abstractmethod
+    def cli(cls, **data: InterpterterModel(**data))-> None:
+        cli(cls, , **data: InterpterterModel(**data))
         
-        # One-off message
-        if message:
-            self.messages.append({"role": "user", "message": message})
-            
-            yield from self._respond()
+    @abstractmethod
+    def __init__(cls)-> None:
+        "Along with setting the configuration it should also check for updates and that check should be pushed into the utility."
 
-            # Save conversation
-            if self.conversation_history:
-                # Check if the directory exists, if not, create it
-                if not os.path.exists(self.conversation_history_path):
-                    os.makedirs(self.conversation_history_path)
-                # Write or overwrite the file
-                with open(os.path.join(self.conversation_history_path, self.conversation_name + '.json'), 'w') as f:
-                    json.dump(self.messages, f)
-                
-            return
-        
-        raise Exception("`interpreter.chat()` requires a display. Set `display=True` or pass a message into `interpreter.chat(message)`.")
+    @abstractmethod
+    def chat(cls, message=None, display=bool, stream=bool)-> List[Dict[str, str]] :
+        "should handle all the chat functionality. consider breaking this into smaller function each responsible for inidivdual tasks."
+        pass
 
-    def _respond(self):
+    @abstractmethod
+     def _streaming_chat(self, message=None, display=True)-> None:
+         """
+         Yields tokens, but also adds them to interpreter.messages. TBH probably would be good to seperate those two responsibilities someday soon
+         Responds until it decides not to run any more code or say anything else.
+         """
+         pass
+     
+    def _respond(self) -> None:
         yield from respond(self)
-            
-    def reset(self):
-        self.messages = []
-        self.conversation_name = datetime.now().strftime("%B %d, %Y")
-        for code_interpreter in self._code_interpreters.values():
-            code_interpreter.terminate()
-        self._code_interpreters = {}
+        
+    @abstractmethod
+    def reset(self) -> None:
+        """Reset the state."""
+        pass
