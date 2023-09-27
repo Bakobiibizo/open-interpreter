7bcf5c3ac76fd68e3141d8caa0ebb94a79a8e48a
Richard Bakobiibizo <110358293+Bakobiibizo@users.noreply.github.com>
2023-09-27 10:16:42 -0700

    Update display_markdown_message.py

--- interpreter/utils/display_markdown_message.py
+++ interpreter/utils/display_markdown_message.py
@@ -1,21 +1,31 @@
+    
 from rich import print as rich_print
 from rich.markdown import Markdown
 from rich.rule import Rule
 
 def display_markdown_message(message):
     """
-    Display markdown message. Works with multiline strings with lots of indentation.
+    Display markdown message. Works with multiline strings with lots of
+indentation.
     Will automatically make single line > tags beautiful.
     """
 
-    for line in message.split("\n"):
-        line = line.strip()
-        if line == "":
-            print("")
-        elif line == "---":
-            rich_print(Rule(style="white"))
-        else:
+for line in message.split("\n"):
+    line = line.strip()
+    if line == "":
+        print("")
+    elif line == "---":
+        rich_print(Rule(style="white"))
+    else:
+        # Try to print the line as Markdown
+        try:
             rich_print(Markdown(line))
+        except UnicodeEncodeError:
+            # If a UnicodeEncodeError is encountered, print the line without Markdown formatting adding this for windows compatibility
+            print(line)
+        except Exception as e:
+            # For any other exceptions, print the exception and the li causing it
+            print(f"An error occurred while printing the following line\n{line}\nError: {str(e)}")
 
     if "\n" not in message and message.startswith(">"):
         # Aesthetic choice. For these tags, they need a space below them
